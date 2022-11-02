46f70d4dcdb6991387cbcd8a745efdd503c8a065
Miguel Arcanjo <945507756m@gmail.com>
2022-11-02 15:30:46 -0300

    bug de apresentar o valor total a pagar arrumado

--- src/multaTransito/Desafio2.java
+++ src/multaTransito/Desafio2.java
@@ -20,33 +20,41 @@ public class Desafio2 {
 		Scanner scanner = new Scanner(System.in);
 		byte qtdMultas;
 		int somaPontosCNH = 0, pontosCNH;
-		double infraLeve = 88.38, infraMedia = 130.16, infraGrave = 195.23, infraGravissima = 293.47; // valor das infrações
-		double valorTotalPagar;
+		double infraLeve = 88.38, infraMedia = 130.16, infraGrave = 195.23, infraGravissima = 293.47; // valor das
+																										// infrações
+		double valorTotalPagar = 0, valorTotalSomado = 0;
+		boolean continuar = false;
 
+		do {
 			System.out.println("Quantas multas você tomou ");
 			qtdMultas = scanner.nextByte();
-			for(int i = 1; i <= qtdMultas; i++) {
+			for (int i = 1; i <= qtdMultas; i++) {
 				System.out.println("Quantos pontos na CNH você recebeu na multa " + i);
 				pontosCNH = scanner.nextInt();
 				somaPontosCNH += pontosCNH;
-				//valor total das multas
-				if(pontosCNH == 3) {
-					valorTotalPagar = infraLeve;
-					valorTotalPagar += valorTotalPagar;
-				}else if(pontosCNH == 4) {
-					valorTotalPagar = infraMedia;
-					valorTotalPagar += valorTotalPagar;
-				}else if(pontosCNH == 5) {
-					valorTotalPagar = infraGrave;
-					valorTotalPagar += valorTotalPagar;
-				}else if(pontosCNH == 7) {
-					valorTotalPagar = infraGravissima;
-					valorTotalPagar += valorTotalPagar;
+				// valor total das multas
+				if (pontosCNH != 3 && pontosCNH != 4 && pontosCNH != 5 && pontosCNH != 7) {
+					System.out.println("Digite uma quantidade valida (3,4,5 ou 7) \n");
+					continuar = true;
 				}
-			}		
-			
-			System.out.println("Ao todo você tomou " + somaPontosCNH + " pontos na carteira");
-			System.out.println("Ao todo você ira pagar R$ " + valorTotalPagar);
+				if (pontosCNH == 3) {
+					valorTotalPagar = infraLeve;
+					valorTotalSomado += valorTotalPagar;
+				} else if (pontosCNH == 4) {
+					valorTotalPagar = infraMedia;
+					valorTotalSomado += valorTotalPagar;
+				} else if (pontosCNH == 5) {
+					valorTotalPagar = infraGrave;
+					valorTotalSomado += valorTotalPagar;
+				} else if (pontosCNH == 7) {
+					valorTotalPagar = infraGravissima;
+					valorTotalSomado += valorTotalPagar;
+				}
+			}
+		} while (continuar);
+
+		System.out.println("Ao todo você tomou " + somaPontosCNH + " pontos na carteira");
+		System.out.println("Ao todo você ira pagar R$ " + valorTotalSomado);
 
 		scanner.close();
 
